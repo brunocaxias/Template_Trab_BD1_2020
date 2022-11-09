fa8182636aeb5fc74ccc9621c032e1ca3edda6fe
Marcos Mendes <57873850+mvmfaria@users.noreply.github.com>
2022-11-08 23:04:24 -0300

    enfermeiro e medico

--- arquivos/inserts.sql
+++ arquivos/inserts.sql
@@ -1,1 +1,7 @@
-wait...
+insert into enfermeiro (nome, cpf, data_nascimento, funcao, coren)
+VALUES ('Julio', '12343256784', '04-03-1987', 'Enfermeiro', '245879SP'),
+	     ('João', '47625034072', '01-11-1999', 'Enfermeiro', '765198ES' );
+       
+insert into medico (nome, cpf, data_nascimento, funcao, crm, especializacao)
+VALUES ('Bruno', '29984145428', '08-04-1981', 'Médico', 'CRM/GO 123456', 'Neurocirurgião'),
+    ('Angela', '34394367589', '12-27-1981', 'Médico', 'CRM/SP 234512', 'Cardiologista');
