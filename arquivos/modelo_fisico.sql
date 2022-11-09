4151c0984d6a27217fe1855167a05d4d3b969cf5
Vinícius Estevam <67986109+vinicius-je@users.noreply.github.com>
2022-11-08 22:54:50 -0300

    update modelo fisico

--- arquivos/modelo_fisico.sql
+++ arquivos/modelo_fisico.sql
@@ -15,8 +15,7 @@ INHERITS (pessoa);
 
 -- Cria a tabela funcionario, herda da tabela pessoa
 CREATE TABLE funcionario(
-	funcao VARCHAR(50),
-	dia_semana int
+	funcao VARCHAR(50)
 )
 INHERITS (pessoa);
 
@@ -101,5 +100,3 @@ CREATE TABLE consulta_medicamento(
   	FOREIGN KEY (id_medicamento) REFERENCES medicamento (id),
  	FOREIGN KEY (id_consulta) REFERENCES consulta (id)
 );
-
-ALTER TABLE FUNCIONARIO ADD CONSTRAINT FK_DIA_SEMANA FOREIGN KEY (dia_semana) REFERENCES dia_semana(id);
